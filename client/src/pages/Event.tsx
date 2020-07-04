66c69012ff50794e0e51f8de0e7c59dc98d87650
cotton <chinoknct@gmail.com>
2020-07-04 16:53:21 +0900

    [update]イベントページにおける出欠表示の修正
    イベントページにて出欠の状況が表示できるように修正しました。

--- client/src/pages/Event.tsx
+++ client/src/pages/Event.tsx
@@ -15,47 +15,98 @@ import { EventContext } from "../App";
 import { DateList } from "../components/ui/DateList";
 
 import "../assets/style/event.scss";
+import { start } from "repl";
+
+const UserList = (props: any) => {
+  let start = Moment(props.data.startDate);
+  let end = Moment(props.data.endDate).add("days", 1);
+  let list: any = [];
+
+  while(start.format("YYYY-MM-DD") != end.format("YYYY-MM-DD")) {
+    let possible_date = props.user.possible.filter((item: any) => {
+      return item === start.format("YYYY-MM-DD");
+    });
+    let subtle_date = props.user.subtle.filter((item: any) => {
+      return item === start.format("YYYY-MM-DD");
+    });
+    let impossible_date = props.user.impossible.filter((item: any) => {
+      return item === start.format("YYYY-MM-DD");
+    });
+    if(!possible_date[0]) {
+      if(!subtle_date[0]) {
+        list.push(<td>✕</td>);
+      } else {
+        list.push(<td>△</td>);
+      }
+    } else {
+      list.push(<td>〇</td>);
+    }
+    start.add('days', 1);
+  }
+  return (<Fragment>{ list }</Fragment>);
+};
 
 const Event: React.FC = () => {
   const { stateEdit, dispatch } = useContext(EventContext);
   const { event } = useParams();
   //contextで認証状態を確認（とりあえず開発中はtureにしてる）
   const [authenticated, setAuthenticated] = useState(true);
+  const [render, setRender] = useState(false);
   const [eventData, setEventData] = useState<any | null>(null);
-
+  
   useEffect(() => {
     // 5ef9d12b5606394cf99f404a
     axios.get(`/api/v1/events/${event}`)
-      .then((result: any) => {
-        const data = result.data[0];
-        console.log(result)
-        setEventData(data.title);
-        dispatch({
-          type: "checkEvent",
-          payload: {
-            title:       data.title,
-            description: data.description,
-            startDate:   data.startDate,
-            endDate:     data.endDate,
-            password:    data.password,
-            user:        data.user
-          }
-        });
+    .then((result: any) => {
+      const data = result.data[0];
+      console.log(result)
+      setEventData(data.title);
+      dispatch({
+        type: "checkEvent",
+        payload: {
+          title:       data.title,
+          description: data.description,
+          startDate:   data.startDate,
+          endDate:     data.endDate,
+          password:    data.password,
+          user:        data.user
+        }
       });
+    });
+    console.log(stateEdit.user);
   }, []);
-  
-  useEffect(() => {
-    console.log(stateEdit);
-    console.log(DateList);
-  }, [ stateEdit ]);
-
-  if(!authenticated) {
-    return (
-      <div></div>
-    );
-  } else {
-    return (
-      <div className="container">
+    
+    useEffect(() => {
+      setRender(true);
+      console.log(Array.isArray(stateEdit.user));
+    }, [ stateEdit ]);
+    
+    const ListRender = () => {
+      const trs: any = [];
+      if(!stateEdit.user) {
+        return (<tr></tr>);
+      } else {
+        stateEdit.user.map((item: any) => {
+          trs.push(
+            <tr>
+              <UserList
+              user={item}
+              data={stateEdit}
+              />
+            </tr>
+          )
+        })
+        return (<Fragment>{ trs }</Fragment>);
+      }
+    };
+    
+    if(!authenticated) {
+      return (
+        <div></div>
+        );
+      } else {
+        return (
+          <div className="container">
         <div>path : {event}</div>
         <div>event title : {stateEdit.title}</div>
         <table className="event-table-wrapper">
@@ -70,18 +121,7 @@ const Event: React.FC = () => {
             <td>b</td>
             <td>b</td>
           </tr>
-          <tr>
-            <th>Sato</th>
-            <td>b</td>
-            <td>b</td>
-            <td>b</td>
-          </tr>
-          <tr>
-            <th>Suzuki</th>
-            <td>b</td>
-            <td>b</td>
-            <td>b</td>
-          </tr>
+          <ListRender />
           </tbody>
         </table>
         <div className="event-join-button">
