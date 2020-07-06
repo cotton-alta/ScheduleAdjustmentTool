cff5180c204483f79e6d1627c6cc7a55f5939177
cotton <chinoknct@gmail.com>
2020-07-06 21:07:52 +0900

    [add]イベントURLのコピペ機能の追加
    確認ページにてイベントURLをクリップボードにコピーする機能を追加しました。

--- client/src/pages/CreateResult.tsx
+++ client/src/pages/CreateResult.tsx
@@ -1,13 +1,42 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import history from "../history";
 
+import "../assets/style/result.scss";
+
 const CreateResult: React.FC = () => {
   const { event } = useParams();
+  const url = "localhost:3000/event/" + event;
+
+  const pasteURL = () => {
+    navigator.clipboard
+      .writeText(url)
+      .then(() => {
+        console.log("コピー完了！");
+      })
+      .catch((err: any) => {
+        console.log(err);
+      });
+  };
+
   return (
-    <div>
-      <p>{ event }</p>
+    <div className="result-wrapper">
+      <div>localhost:3000/event/{ event }</div>
+      <div 
+        className="result-button"
+        onClick={pasteURL}
+      >
+        クリップボードへコピー
+      </div>
+      <div className="result-button">
+        <Link
+          className="result-link"
+          to={`/event/${event}`}
+        >
+          イベントページへ！
+        </Link>
+      </div>
     </div>
   )
 };
